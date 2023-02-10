6e3c1a53552f534866be76e6e3cc0d96e3ae0f9b
eugenio <eugenio.varetti@mail.polimi.it>
2023-02-10 23:03:44 +0100

    executable cleaning (timeseries dataset now is ts.csv, covariates dataset now is coord.csv)

--- examples/ar1nig_hierarchy/runsingle.sh
+++ examples/ar1nig_hierarchy/runsingle.sh
@@ -4,8 +4,8 @@ build/run_mcmc \
     --hier-type AR1NIG --hier-args examples/ar1nig_hierarchy/in/hyperh_unito.asciipb \
     --mix-type DPC2 --mix-args examples/ar1nig_hierarchy/in/dp.asciipb \
     --coll-name examples/ar1nig_hierarchy/out/chains.recordio \
-    --data-file resources/datasets/ts_lombardia.csv \
-    --mix-cov-file resources/datasets/coord_lombardia.csv \
+    --data-file resources/datasets/ts.csv \
+    --mix-cov-file resources/datasets/coord.csv \
     --n-cl-file examples/ar1nig_hierarchy/out/numclust.csv \
     --clus-file examples/ar1nig_hierarchy/out/clustering.csv \
     --clus-state-params-file examples/ar1nig_hierarchy/out/stateparams.csv
